2f23c5bc8260ec6f841ff5a80e9fe09dd22de82c
AfsanehDB <125228754+AfsanehDB@users.noreply.github.com>
2023-02-22 17:07:34 -0600

    Add files via upload

---
+++ loan-analyzer.py
@@ -0,0 +1,26 @@
+# list of loan cost 
+loan_costs = [500, 600, 200, 1000, 450]
+number_of_loans = len(loan_costs)
+total_of_all_loans = sum(loan_costs)
+average_loan_price = total_of_all_loans/number_of_loans
+print(" total number of loans is:", number_of_loans)
+print("total of all loans is", total_of_all_loans)
+print("average loan price is", average_loan_price)
+#add part 3 analyze 
+
+calculate_present_value = future_value/(1+annual_discount_rate/12)**months 
+def present_value():
+  annual_discount_rate = 0.20
+  present_value = calculate_present_value(
+    new_loan["future_value"],
+    new_loan["remaining_months"],
+    annual_discount_rate)
+inexpensive_loans = []
+for laons in new_loan: 
+  if loan_costs <= 500:
+    inexpensive_loans = [new_loan], 
+print("inexpensive_loans are", inexpensive_loans)
+# complete write csv 
+  
+    
+      
