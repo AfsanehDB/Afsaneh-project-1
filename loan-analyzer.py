4c1904dbc3583d3966056c35e1e355ea5bbf3fbc
AfsanehDB <125228754+AfsanehDB@users.noreply.github.com>
2023-02-22 17:53:14 -0600

    Add files via upload

--- loan-analyzer.py
+++ loan-analyzer.py
@@ -7,19 +7,28 @@ print(" total number of loans is:", number_of_loans)
 print("total of all loans is", total_of_all_loans)
 print("average loan price is", average_loan_price)
 #add part 3 analyze 
-
-calculate_present_value = future_value/(1+annual_discount_rate/12)**months 
-def present_value():
+future_value = loan.get (future_value)
+remaining_months = loan.get(remaining_months)
+print ("future value of this loan is", future_value)
+print ("remaining monthes of this loan is" remaining_months)
+calculate_present_value = future_value /(1 + annual_discount_rate / 12) ** months 
+def present_value(future_value, remaining_months, annual_discount_rate):
   annual_discount_rate = 0.20
   present_value = calculate_present_value(
     new_loan["future_value"],
     new_loan["remaining_months"],
     annual_discount_rate)
+  return (present_value)
+if present_value > = loan_costs:
+  print( "The loan is worth at least the cost to buy it")
+else
+  print(" The loan is too expencive and not worth the price.")
+
 inexpensive_loans = []
 for laons in new_loan: 
   if loan_costs <= 500:
-    inexpensive_loans = [new_loan], 
-print("inexpensive_loans are", inexpensive_loans)
+    inexpensive_loans.append [new_loan], 
+print("inexpensive_loans are", inexpensive_loans[])
 # complete write csv 
   
     
